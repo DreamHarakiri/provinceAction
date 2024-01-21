0f5ebbbe068464ecb0b4bc8bf0e87c41fa04afb5
DreamHarakiri <derek.gaming744@gmail.com>
2024-01-22 01:34:41 +0300

    start menu func

---
+++ actions/assets/script/menu.js
@@ -0,0 +1,10 @@
+const exitBtn = document.querySelector(".exitBtn");
+
+const exitAccount = () => {
+  localStorage.clear("AccountData");
+  window.location.href = "../../../index.html";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  exitBtn.addEventListener("click", exitAccount);
+});
